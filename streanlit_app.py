eaec1f5cd25733d96806e6e9b313a1fdef287769
Payal Verma <115793104+Payal1980@users.noreply.github.com>
2022-10-18 13:50:09 +0530

    Update streanlit_app.py
    Update as per given in the course

--- streanlit_app.py
+++ streanlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast menu')
 streamlit.text('Blueberry and Omega 3')
-streamlit.text('Kale and spinach')
+streamlit.text('Kale and spinach Rocket smoothie')
 streamlit.text('Hard boiled free range egg')
