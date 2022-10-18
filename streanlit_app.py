a9e46b8dae4396734e6f326edcac45bdd94add61
Payal Verma <115793104+Payal1980@users.noreply.github.com>
2022-10-18 13:48:11 +0530

    Update streanlit_app.py
    Made titles look like titles !

--- streanlit_app.py
+++ streanlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast menu')
 streamlit.text('Blueberry and Omega 3')
-streamlit.title('Kale and spinach')
-streamlit.title('Hard boiled free range egg')
+streamlit.text('Kale and spinach')
+streamlit.text('Hard boiled free range egg')
