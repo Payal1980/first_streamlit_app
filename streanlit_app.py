9dee6133f052210db75ef6d48abcc46066c6799a
Payal Verma <115793104+Payal1980@users.noreply.github.com>
2022-10-18 13:47:33 +0530

    Update streanlit_app.py
    Added breakfast items

--- streanlit_app.py
+++ streanlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Blueberry and Omega 3')
+streamlit.title('Kale and spinach')
+streamlit.title('Hard boiled free range egg')
