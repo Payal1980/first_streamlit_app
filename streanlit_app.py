e717d49ab11c72a575c9378ffcc49f22f07a86a9
Payal Verma <115793104+Payal1980@users.noreply.github.com>
2022-10-18 15:53:06 +0530

    Update streanlit_app.py
    Add emojis

--- streanlit_app.py
+++ streanlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast menu')
-streamlit.text('Blueberry and Omega 3')
-streamlit.text('Kale and spinach Rocket smoothie')
-streamlit.text('Hard boiled free range egg')
+streamlit.title('🥣 My parents new healthy diner 🥣')
+streamlit.header('🍓🍅 Breakfast menu')
+streamlit.text('🍇 Blueberry and Omega 3')
+streamlit.text('🥤 Kale and spinach Rocket smoothie')
+streamlit.text('🍚 Hard boiled free range egg')
