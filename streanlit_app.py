d1b625ef07e707a05639380b87434233f9315d44
Payal Verma <115793104+Payal1980@users.noreply.github.com>
2022-10-20 16:52:03 +0530

    Update streanlit_app.py
    Set indexing by fruits

--- streanlit_app.py
+++ streanlit_app.py
@@ -7,6 +7,7 @@ streamlit.text('🥤 Kale and spinach Rocket smoothie')
 streamlit.text('🍚 Hard boiled free range egg')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Let's put a pick list 
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+#streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+my_fruit_list = my_fruit_list.set_index('Fruit')
 # Display the list on the page
 streamlit.dataframe(my_fruit_list)
