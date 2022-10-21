926c275e044f79023efacef088ed273c0087a830
Payal Verma <115793104+Payal1980@users.noreply.github.com>
2022-10-21 15:07:41 +0530

    Update streanlit_app.py

--- streanlit_app.py
+++ streanlit_app.py
@@ -8,7 +8,7 @@ streamlit.text('🍚 Hard boiled free range egg')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Let's put a pick list 
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+#fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the list on the page
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(my_fruit_list)
