6570ef917afcd0a85920d7a45a6c3a73544d3d7c
Payal Verma <115793104+Payal1980@users.noreply.github.com>
2023-01-11 15:27:44 +0530

    Update streanlit_app.py

--- streanlit_app.py
+++ streanlit_app.py
@@ -3,7 +3,7 @@ import pandas
 streamlit.title('🥣 My parents new healthy diner 🥣')
 streamlit.header('🍓🍅 Breakfast menu')
 streamlit.text('🍇 Blueberry and Omega 3')
-streamlit.text('🥤 Kale and spinach Rocket smoothie')
+streamlit.text('🥤 Kale and spinach Rocket  smoothie')
 streamlit.text('🍚 Hard boiled free range egg')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Let's put a pick list 
