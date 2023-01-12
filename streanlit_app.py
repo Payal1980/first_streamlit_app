1ddedbd3ce3fe9e12df18fff074e1a954bedf451
Payal Verma <115793104+Payal1980@users.noreply.github.com>
2023-01-12 14:23:30 +0530

    Update streanlit_app.py

--- streanlit_app.py
+++ streanlit_app.py
@@ -14,8 +14,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the list on the page
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json())
 # get the json normalised form
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write out the normalised data in the form of a table
