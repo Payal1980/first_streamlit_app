d48f4b2c498c54db185dcdaf19d127f1d8385c25
Payal Verma <115793104+Payal1980@users.noreply.github.com>
2023-01-13 15:47:23 +0530

    Update streanlit_app.py

--- streanlit_app.py
+++ streanlit_app.py
@@ -73,6 +73,6 @@ if streamlit.button('Add a fruit to the list'):
   my_cnx.close()
   streamlit.text(back_from_function)
   
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
